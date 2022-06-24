21019b81535153f24976282b8d54d873dce60db8
Diablo <80642969+progzone122@users.noreply.github.com>
2022-06-24 19:02:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
+let express = require('express')
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
